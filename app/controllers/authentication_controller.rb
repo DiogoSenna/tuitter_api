eec80afd5b40d9c334dcd50705e83c7b894a8cfd
Diogo Senna <disenna@gmail.com>
2024-09-08 21:52:04 -0300

    refactor(authentication_controller): improve user query

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,16 +1,15 @@
 class AuthenticationController < ApplicationController
   # POST /auth/login
   def login
-    @user = User.find_by_username(params[:username])
-    @user = User.find_by_email(params[:email]) unless @user
+    @user = User.where(username: params[:username])
+                .or(User.where(email: params[:email]))
+                .first
 
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
 
       render json: {
         token: token,
-        expiration: time.strftime("%m-%d-%Y %H:%M"),
         username: @user.username
       }, status: :ok
     else
