1e129e99821d70e55929a3f072a5590836a361d8
Diogo Senna <disenna@gmail.com>
2024-09-08 21:51:28 -0300

    feat: add login route & not_found to nonexistent routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get '/*a', to: 'application#not_found'
   resources :users, param: :username
+  post '/auth/login', to: 'authentication#login'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
