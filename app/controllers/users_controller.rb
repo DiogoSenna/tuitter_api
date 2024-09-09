5661926a0e0aed95d0534096e8ed9cab831dc815
Diogo Senna <disenna@gmail.com>
2024-09-08 21:00:59 -0300

    feat(users_controller): add authorize_request before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authorize_request, except: :create
   before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
@@ -41,7 +42,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find_by!(username: params[:username])
+      @user = User.find_by_username!(params[:username])
     end
 
     # Only allow a list of trusted parameters through.
