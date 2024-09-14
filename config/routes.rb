73270da9405037d393a766610b28a330599cc68b
Diogo Senna <disenna@gmail.com>
2024-09-14 03:39:58 -0300

    feat: add users & profiles routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   delete '/auth/logout', to: 'authentication#logout'
 
+  resources :users, param: :username do
+    resource :profile, only: %i[show create update]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
