570d849f1613c8f9b1b8683f9fcc502ebb1e7e60
Diogo Senna <disenna@gmail.com>
2024-09-28 18:36:57 -0300

    feat(profile.rb): implement profile validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, :last_name, :birth_date, presence: true
+  validates :country, :city, format: { with: /\A[a-zA-Z\s]+\z/, message: 'only letters are allowed' }
+
+  validate :valid_country
+
+  private
+
+  def valid_country
+    valid_countries = ISO3166::Country.all.map(&:iso_short_name)
+
+    errors.add(:country, 'not a valid country') unless valid_countries.include?(country)
+  end
 end
