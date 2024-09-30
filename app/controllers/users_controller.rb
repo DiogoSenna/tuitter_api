843fe6d386cfdb088ecc9f0eee193b4b7f0ab125
Diogo Senna <disenna@gmail.com>
2024-09-29 22:28:29 -0300

    feat(users_controller): separate user_params from new_user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(new_user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.save
+      render json: user, status: :created, location: user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
@@ -39,7 +39,11 @@ class UsersController < ApplicationController
       @user = User.find_by!(username: params[:username])
     end
 
-    def user_params
+    def new_user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email)
     end
 end
