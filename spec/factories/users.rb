973320ba2f6102773237abb0afa8e87e462d06da
Diogo Senna <disenna@gmail.com>
2024-09-29 22:25:38 -0300

    test(factories/users.rb): enable special_characters option in password field

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     username { Faker::Internet.username }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(special_characters: true) }
   end
 end
