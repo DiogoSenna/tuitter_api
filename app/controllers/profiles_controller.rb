ce86c710de9e17eea6aebaba679c2890574ca0b7
Diogo Senna <disenna@gmail.com>
2024-10-23 22:54:01 -0300

    feat: add state field in profile model & controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -30,6 +30,6 @@ class ProfilesController < ApplicationController
     end
 
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :birth_date, :city, :country)
+      params.require(:profile).permit(:first_name, :last_name, :birth_date, :city, :country, :state)
     end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,15 +2,23 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   validates :first_name, :last_name, :birth_date, presence: true
-  validates :country, :city, format: { with: /\A[a-zA-Z\s]+\z/, message: 'only letters are allowed' }
+  validates :country, :state, :city, format: { with: /\A[a-zA-Z\s]+\z/, message: 'only letters are allowed' },
+            presence: true
 
   validate :valid_country
+  validate :valid_state
 
   private
 
   def valid_country
-    valid_countries = ISO3166::Country.all.map(&:iso_short_name)
+    valid_countries = ISO3166::Country.all.map(&:alpha2)
 
     errors.add(:country, 'not a valid country') unless valid_countries.include?(country)
+  end
+
+  def valid_state
+    unless ISO3166::Country[country]&.subdivisions&.key?(state)
+      errors.add(:state, 'not a valid state for the selected country')
+    end
   end
 end
