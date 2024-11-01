5efb0976c98827d9432f32ee4981497b5ccbf874
Diogo Senna <disenna@gmail.com>
2024-10-31 22:07:26 -0300

    feat: add unique index & fk constraints to roles_users join table

--- db/migrate/20241031031622_create_roles_users_join_table.rb
+++ db/migrate/20241031031622_create_roles_users_join_table.rb
@@ -1,5 +1,10 @@
 class CreateRolesUsersJoinTable < ActiveRecord::Migration[7.1]
   def change
-    create_join_table :roles, :users
+    create_join_table :roles, :users do |t|
+      t.index [:role_id, :user_id], unique: true
+
+      t.foreign_key :roles
+      t.foreign_key :users
+    end
   end
 end
