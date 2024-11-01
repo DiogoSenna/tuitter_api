7dc56b81cd27d14bef503351308617f33fe86f86
Diogo Senna <disenna@gmail.com>
2024-10-31 22:08:31 -0300

    feat: add roles and users seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Role::DEFAULT.values.each do |role_name|
+  Role.find_or_create_by!(name: role_name)
+end
+
+admin = User.find_or_create_by!(email: 'admin@example.com') do |user|
+  user.username = 'user_admin'
+  user.password = "aAbB12!@"
+  user.password_confirmation = "aAbB12!@"
+end
+
+admin_role = Role.find_by!(name: Role::DEFAULT[:admin])
+admin.roles << admin_role unless admin.roles.include?(admin_role)
