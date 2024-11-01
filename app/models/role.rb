e48922619b45de2653c8f3f159427a9abe31f6c0
Diogo Senna <disenna@gmail.com>
2024-10-31 22:08:56 -0300

    chore(role.rb): add model DEFAULT constant

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,11 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :users
+
+  DEFAULT = {
+    admin: "administrator",
+    user: "regular_user",
+    premium: "premium_user",
+    company: "company",
+  }
+
 end
