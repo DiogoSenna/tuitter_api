e3bdcd043f5852ac0ba830bbb2b5382aae4260fb
Diogo Senna <disenna@gmail.com>
2024-11-09 20:57:06 -0300

    chore(users_controller.rb): remove unused method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :authorize_request, except: :create
   before_action :set_user, only: %i[ show update destroy ]
-  authorize_resource
 
   def index
     @users = User.all
