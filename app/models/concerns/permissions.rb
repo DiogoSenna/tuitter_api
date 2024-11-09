95114572ca09cacd3484614069433efd482ac015
Diogo Senna <disenna@gmail.com>
2024-11-09 20:44:46 -0300

    feat: introduce roles & permissions concerns

---
+++ app/models/concerns/permissions.rb
@@ -0,0 +1,33 @@
+module Permissions
+  USERS = %i[users_update users_delete].freeze
+  PROFILES = %i[profiles_show profiles_update].freeze
+  ROLES = %i[roles_index roles_show roles_create roles_update roles_delete].freeze
+
+  ALL = USERS + PROFILES + ROLES
+
+  def self.resolve_permission(permission)
+    dynamic_alias = resolve_alias(permission)
+
+    return dynamic_alias unless dynamic_alias.nil?
+
+    permission
+  end
+
+  private
+
+  def self.resolve_alias(name)
+    case name.to_s
+    when /_read\z/
+      base_name = name.to_s.sub(/_read\z/, '')
+      ALL.select { |permission| permission.to_s.match?(/\A#{base_name}_(index|show)\z/) }
+    when /_write\z/
+      base_name = name.to_s.sub(/_write\z/, '')
+      ALL.select { |permission| permission.to_s.match?(/\A#{base_name}_(create|update)\z/) }
+    when /_manage\z/
+      base_name = name.to_s.sub(/_manage\z/, '')
+      ALL.select { |permission| permission.to_s.match?(/\A#{base_name}_(index|show|create|update|delete)\z/) }
+    else
+      nil
+    end
+  end
+end
