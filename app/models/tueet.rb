e621bfbd147928da7b36ca5ccdbc5df461bb15d5
Diogo Senna <disenna@gmail.com>
2024-11-30 01:01:40 -0300

    fix(tueet.rb): user association access
    - Extract content length validation to a separate method in Tueet model
    - Improve readability and maintainability of content length validation

--- app/models/tueet.rb
+++ app/models/tueet.rb
@@ -3,8 +3,17 @@ class Tueet < ApplicationRecord
   belongs_to :parent, class_name: 'Tueet', optional: true
   has_many :replies, class_name: 'Tueet', foreign_key: 'parent_id', dependent: :destroy
 
-  validates :content, presence: true, length: { maximum: user.admin? || user.premium? ? 4000 : 280 }
+  validates :content, presence: true
+  validate :validate_content_length
 
   scope :root_tueets, -> { where(parent_id: nil) }
   scope :reply_tueets, -> { where.not(parent_id: nil) }
+
+  private
+
+  def validate_content_length
+    max_length = user.admin? || user.premium? ? 4000 : 280
+
+    errors.add(:content, "is too long (maximum is #{max_length} characters)") if content.length > max_length
+  end
 end
