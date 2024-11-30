3d9ab4bd187d155c6959119d7bdcc244e1ad8f8f
Diogo Senna <disenna@gmail.com>
2024-11-30 20:17:01 -0300

    test: add tueet factory

--- spec/factories/tueets.rb
+++ spec/factories/tueets.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :tueet do
-    content { "MyText" }
-    user { nil }
+    content { Faker::Lorem.paragraph_by_chars(number: 280) }
+    parent_id { nil }
+    association :user, factory: :user
   end
 end
