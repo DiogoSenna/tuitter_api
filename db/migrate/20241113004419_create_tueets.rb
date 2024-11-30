474b86f7af8b960fab2537bbf5e294beb6004759
Diogo Senna <disenna@gmail.com>
2024-11-30 00:02:01 -0300

    chore: add parent reference to tueets' table migration

--- db/migrate/20241113004419_create_tueets.rb
+++ db/migrate/20241113004419_create_tueets.rb
@@ -3,6 +3,7 @@ class CreateTueets < ActiveRecord::Migration[7.1]
     create_table :tueets do |t|
       t.text :content, null: false
       t.references :user, null: false, foreign_key: true
+      t.references :parent, foreign_key: { to_table: :tueets }
 
       t.timestamps
     end
